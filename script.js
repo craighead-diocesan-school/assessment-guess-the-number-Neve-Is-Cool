343e9e8db13ec27d6a4ee80ef4e35364853a5594
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-08-20 10:59:32 +1200

    Expected Testing
    I tested code with units of '1' '2' '3' '4' '5' '6' '7' '8' '9' '10' '11' '12' '13' '14' '15' '16' '17' '18' '19' & '20'. This came with the response, 'Nope, try again' until the guess of '20' which was the number generated, this gave the response of 'Congrats, you got it!!'. Also alerted a message of the users name 'Neve' and the number of attempts being '20'.

--- script.js
+++ script.js
@@ -9,27 +9,36 @@ function guessTheNumber() {
 
     const randomGeneratorRange = 20
     let theNumber = Math.floor (Math.random() *randomGeneratorRange)
+    theNumber = theNumber + 1
 
+    alert(theNumber)
 
+    
     //Generates a random number between 1 and 20 inclusive.
-
+    
     let numOfAttempts = 0
+    let usersAttempt = 0
     //
     alert ('Hi ' + usersName + '! lets play a game of guess the number! Try to guess a number between 1 and 20 inclusive in the fewest guesses possible.')
-
-    while (usersAttempt =! theNumber ) {
-       let usersAttempt = prompt ('What is your guess?')
-
-        if (usersAttempt =! theNumber) {
-            alert ('Nope, try again.')
-            numOfAttempts = numOfAttempts + 1
-        } else if (usersAttempt == theNumber) {
+    
+    while (usersAttempt != theNumber ) {
+        usersAttempt = prompt ('What is your guess?')
+        usersAttempt = usersAttempt.trim()
+        usersAttempt = Number (usersAttempt)
+        
+        if (usersAttempt == theNumber) {
             alert ('Congrats, you got it!!')
             numOfAttempts = numOfAttempts + 1
+        } else if (usersAttempt > 20 || usersAttempt < 1) {
+            alert ('The number you guessed in not within a range of 1-20')
+        } else if (usersAttempt != theNumber) {
+            alert ('Nope, try again.')
+            numOfAttempts = numOfAttempts + 1 
         } else {
             alert ('Invalid attempt, please try again.')
         }
     }
 
+    alert (usersName + ' = ' + numOfAttempts)
 
 }
