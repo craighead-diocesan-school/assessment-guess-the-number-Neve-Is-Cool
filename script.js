651d45044821c672026de803c57aace0a22530bf
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-08-27 10:47:58 +1200

    Tested and debugged the program
    Tested the program by changing the min and max to a different range, fully functional and could be easily changed to shift the purpose of the game.

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function guessTheNumber() {
     //Sets both to 0 so they can be updated through the course of the game.
     //Sets the maximum and minimum ranges for comparisons to determine the output following their attempts.
 
-    alert ('Hi ' + usersName + '! lets play a game of guess the number! Try to guess a number between 1 and 20 inclusive in the fewest guesses possible.')
+    alert ('Hi ' + usersName + '! lets play a game of guess the number! Try to guess a number between ' + min + ' and ' + max + ' inclusive in the fewest guesses possible.')
     //Explains the game to the user and includes their name in the greeting.
     
     while (usersAttempt != theNumber ) {
@@ -35,7 +35,7 @@ function guessTheNumber() {
             boardOfScores.push('\n' + usersName + '=' + numOfAttempts)
             //When the usersAttempt is equal to the random generated number, one attempt is added to the total number of attempts and the usersName and final number of attempts is pushed to a new line within the boardOfScores array.
         } else if (usersAttempt > max || usersAttempt < min) {
-            alert ('Sorry, the number you guessed in not within a range of 1-20, please try again')
+            alert ('Sorry, the number you guessed in not within a range of ' + min + '-' + max +  ', please try again')
             //If the users attempt is not within the range of 1-20 this message will be displayed and no new attempts will be added to the total number of attempts.
         } else if (usersAttempt != theNumber && usersAttempt >= min && usersAttempt <= max ) {
             alert ('Nope, try again.')
